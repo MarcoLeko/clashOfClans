b5db39c380d77f50aee02fc9df44406b03256ad0
Marco Leko <Unterschied1>
2018-02-26 20:37:06 +0100

    fixed http unit test in player search service with the httpTestingcontroller

--- src/app/home/services/player-search/player-search.service.spec.ts
+++ src/app/home/services/player-search/player-search.service.spec.ts
@@ -2,37 +2,39 @@ import {inject, TestBed} from '@angular/core/testing';
 
 import {PlayerSearchService} from './player-search.service';
 import {HashTransformerService} from "../../../shared/domain/hash-transformer.service";
-import {XHRBackend} from "@angular/http";
-import {MockBackend} from "@angular/http/testing";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Mocks} from "../../../testing/mocks";
 
 describe('PlayerSearchService', () => {
 
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
       providers: [
         PlayerSearchService,
-        HashTransformerService,
-        {provide: XHRBackend, useClass: MockBackend}
+        HashTransformerService
       ],
     });
+
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', inject([PlayerSearchService], (service: PlayerSearchService) => {
     expect(service).toBeTruthy();
   }));
 
-  // it('should return observable of PlayerByPlayerTag obj', inject([PlayerSearchService, XHRBackend], (service, mockBackend) => {
-  //   const mockResponse: PlayerByPlayerTagType = Mocks.PLAYERSTATSBYPLAYERTAG;
-  //
-  //   mockBackend.connections.subscribe(connection => {
-  //     connection.mockRespond(new Response(new ResponseOptions({
-  //       body: JSON.stringify(mockResponse)
-  //     })));
-  //   });
-  //   service.getPlayer(Mocks.PLAYERTAG).subscribe(player => {
-  //     expect(true).toBe(false);
-  //   })
-  // }));
+  it('should return observable of PlayerByPlayerTag', inject([PlayerSearchService, HashTransformerService], (service, hashTransformer) => {
+    service.getPlayer(Mocks.PLAYERTAG).subscribe(data => {
+      expect(data).toEqual(Mocks.PLAYERSTATSBYPLAYERTAG);
+    });
+    
+    const req = httpMock.expectOne(PlayerSearchService.PLAYERURL + hashTransformer.transformHash(Mocks.PLAYERTAG));
+    expect(req.request.method).toBe("GET");
+    req.flush(Mocks.PLAYERSTATSBYPLAYERTAG);
+  }));
 });

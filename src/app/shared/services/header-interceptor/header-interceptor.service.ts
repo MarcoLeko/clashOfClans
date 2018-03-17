a532111dcf7b069937e9cd36db807b0564ec94fd
MarcoLeko <l.__marco@hotmail.de>
2018-03-17 18:27:24 +0100

    fixed display issue and need to add ngx bootstrap for pop up modals

--- src/app/shared/services/header-interceptor/header-interceptor.service.ts
+++ src/app/shared/services/header-interceptor/header-interceptor.service.ts
@@ -1,11 +1,11 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HeaderInterceptorService implements HttpInterceptor {
 
-  private static TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFjNDlmZWQ2LWVhNTctNDMyZS04MmI2LTAyOTQzNjQ1NWM2OCIsImlhdCI6MTUyMTE4NzQ3Nywic3ViIjoiZGV2ZWxvcGVyLzFjZTE4NzU2LTNkMjAtZjlhZi1lOWVkLTg2YmU4MGEzYjdlMiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjg0LjE4NS41Ni4xMTMiXSwidHlwZSI6ImNsaWVudCJ9XX0.BG7bxdObc6Tr_3eTvToKYWZTO_jEKywQDlVqBbv8jwu_f359JMYOnMPo7PNmvJfQZbC_zMXgl5P_NqL75m-_Tw';
+  private static TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBkNTM2OTM1LTlkOTgtNDE5OC04NjM5LTBkNDMyNjc4NDgxZSIsImlhdCI6MTUyMTI5NzM1Mywic3ViIjoiZGV2ZWxvcGVyLzFjZTE4NzU2LTNkMjAtZjlhZi1lOWVkLTg2YmU4MGEzYjdlMiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkyLjc1Ljc4LjIxNSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.2DxxAyY4yESzLxHp5aQq9jF8zbkw2TS2yvd-Ha-HBIgfvUaQRERkYOQYq03vActT7zVP8x7VD4PwBeE1Zdb8yg';
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 

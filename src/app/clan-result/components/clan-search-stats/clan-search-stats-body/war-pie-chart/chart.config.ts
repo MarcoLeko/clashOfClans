bec33690b9646fcb1fe25c5af6ba7713e3cf989b
Marco Leko <Unterschied1>
2018-05-25 10:31:56 +0200

    war pie chart comp refactoring shift responsibilities in their own classes and some ui adaption - overall happy ! :)

---
+++ src/app/clan-result/components/clan-search-stats/clan-search-stats-body/war-pie-chart/chart.config.ts
@@ -0,0 +1,90 @@
+import * as pattern from 'patternomaly';
+
+export class ChartConfig {
+  private chartOptions: any;
+  private chartData: number[];
+  private chartLabels: string[];
+  private chartDefaultColors: any[] = [{
+    backgroundColor: ['#2bc741', '#c70007', '#c7c4c5'],
+    borderColor: ['#474B43', '#474B43', '#474B43'],
+    borderWidth: [2, 2, 2]
+  }];
+  private chartColorBlindColors: any[] = [{
+    backgroundColor: [
+      pattern.draw('square', '#ff6384'),
+      pattern.draw('circle', '#36a2eb'),
+      pattern.draw('diamond', '#cc65fe'),
+    ],
+    borderColor: ['#474B43', '#474B43', '#474B43'],
+    borderWidth: [2, 2, 2]
+  }];
+  private chartType: 'line' | 'pie' | 'doughnut' | 'bar' | 'radar' | 'polarArea' | 'bubble' = 'doughnut';
+  public isColorBlindnessActivated: boolean = false;
+
+  constructor(totalValue: number) {
+    this.chartOptions = {
+      cutoutPercentage: 40,
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+      legend: {
+        position: 'right',
+        labels: {
+          padding: 20,
+          fontSize: 20,
+          fontColor: '#212529'
+        }
+      },
+      tooltips: {
+        callbacks: {
+          label(tooltipItem, data) {
+            let label: string = data.labels[tooltipItem.index];
+            if (label) {
+              label = label.slice(0, label.indexOf(':') + 1).toString();
+              const percentage: string = ((data.datasets[0].data[tooltipItem.index] * 100) / totalValue).toFixed(2);
+              label += percentage + '%';
+            }
+            return label;
+          }
+        }
+      }
+    };
+  }
+
+  getChartOptions(): any {
+    return this.chartOptions;
+  }
+
+  public setData(data: number[]): void {
+    this.chartData = data;
+  }
+
+  public getData(): number[] {
+    return this.chartData;
+  }
+
+  public setLabels(labels: string[]): void {
+    this.chartLabels = labels;
+  }
+
+  public getLabels(): string[] {
+    return this.chartLabels;
+  }
+
+  public getType(): 'line' | 'pie' | 'doughnut' | 'bar' | 'radar' | 'polarArea' | 'bubble' {
+    return this.chartType;
+  }
+
+  public getDefaultColors(): any[] {
+    return this.chartDefaultColors;
+  }
+
+  public getColorBlindColors(): any[] {
+    return this.chartColorBlindColors;
+  }
+}
--- src/app/clan-result/components/clan-search-stats/clan-search-stats-body/war-pie-chart/war-pie-chart.component.ts
+++ src/app/clan-result/components/clan-search-stats/clan-search-stats-body/war-pie-chart/war-pie-chart.component.ts
@@ -1,92 +1,40 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ClansByClantagType} from '../../../../../../generated/types';
-import * as pattern from 'patternomaly';
+import {ChartConfig} from './chart.config';
 
 @Component({
   selector: 'app-war-pie-chart',
   templateUrl: './war-pie-chart.component.html',
   styleUrls: ['./war-pie-chart.component.css']
 })
+
 export class WarPieChartComponent implements OnInit {
 
   @Input() clanResult: ClansByClantagType;
-
-  public pieChartOptions: any;
-  public pieChartData: number[] = [];
-  public pieChartLabels: string[] = [];
-  public pieChartType: string = 'pie';
-
-  public isColorBlindnessActivated = false;
-  public pieChartDefaultColors: any[] = [{
-    backgroundColor: ['#2bc741', '#c70007', '#c7c4c5'],
-    borderColor: ['#474B43', '#474B43', '#474B43'],
-    borderWidth: [2, 2, 2]
-  }];
-
-  public pieChartColorBlindColors: any[] = [{
-    backgroundColor: [
-      pattern.draw('square', '#ff6384'),
-      pattern.draw('circle', '#36a2eb'),
-      pattern.draw('diamond', '#cc65fe'),
-    ],
-    borderColor: ['#474B43', '#474B43', '#474B43'],
-    borderWidth: [2, 2, 2]
-  }];
-
+  public pieChartConfig: ChartConfig;
 
   ngOnInit() {
-    this.configurePieChartLegend();
+    this.pieChartConfig = new ChartConfig(this.calculateWarParticipations());
     this.configurePieChartData();
-    const totalWar: number = this.calculateWarParticipations();
-    this.pieChartOptions = {
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        }
-      },
-      legend: {
-        position: 'right',
-        labels: {
-          padding: 20,
-          fontSize: 20,
-          fontColor: '#212529'
-        }
-      },
-      tooltips: {
-        callbacks: {
-          label(tooltipItem, data) {
-            let label: string = data.labels[tooltipItem.index];
-            if (label) {
-              label = label.slice(0, label.indexOf(':') + 1).toString();
-              const percentage: string = ((data.datasets[0].data[tooltipItem.index] * 100) / totalWar).toFixed(2);
-              label += percentage + '%';
-            }
-            return label;
-          }
-        }
-      }
-    };
-  }
-
-  public configurePieChartLegend() {
-    if (!this.clanResult.isWarLogPublic) {
-      this.pieChartData = [this.clanResult.warWins, 0, 0];
-    }
-    else {
-      this.pieChartData = [this.clanResult.warWins, this.clanResult.warLosses, this.clanResult.warTies];
-    }
+    this.configurePieChartLegend();
   }
 
   public configurePieChartData() {
     if (!this.clanResult.isWarLogPublic) {
-      this.pieChartLabels = ['Wins: ' + `${this.clanResult.warWins}`, 'Losses: Not public', 'Ties: Not public'];
+      this.pieChartConfig.setData([this.clanResult.warWins, 0, 0]);
     }
     else {
-      this.pieChartLabels = ['Wins: ' + `${this.clanResult.warWins}`, 'Losses: ' + `${this.clanResult.warLosses}`,
-        'Ties: ' + +`${this.clanResult.warTies}`];
+      this.pieChartConfig.setData([this.clanResult.warWins, this.clanResult.warLosses, this.clanResult.warTies]);
+    }
+  }
+
+  public configurePieChartLegend() {
+    if (!this.clanResult.isWarLogPublic) {
+      this.pieChartConfig.setLabels(['Wins: ' + `${this.clanResult.warWins}`, 'Losses: Not public', 'Ties: Not public']);
+    }
+    else {
+      this.pieChartConfig.setLabels(['Wins: ' + `${this.clanResult.warWins}`, 'Losses: ' + `${this.clanResult.warLosses}`,
+        'Ties: ' + +`${this.clanResult.warTies}`]);
     }
   }
 
@@ -98,10 +46,10 @@ export class WarPieChartComponent implements OnInit {
   }
 
   backgroundOnChanges() {
-    if (!this.isColorBlindnessActivated) {
-      return this.pieChartDefaultColors;
+    if (!this.pieChartConfig.isColorBlindnessActivated) {
+      return this.pieChartConfig.getDefaultColors();
     } else {
-      return this.pieChartColorBlindColors;
+      return this.pieChartConfig.getColorBlindColors();
     }
   }
 }

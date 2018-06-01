6b11a9c319f40c9dc8b701e621da6bace5cb5d9c
Marco Leko <Unterschied1>
2018-06-01 10:13:50 +0200

    ngc build error fix

--- src/app/clan-result/clan-result.module.ts
+++ src/app/clan-result/clan-result.module.ts
@@ -11,7 +11,6 @@ import {IsWarLogPublicPipe} from './pipes/is-war-log-public/is-war-log-public.pi
 import {WarPieChartComponent} from './components/clan-search-stats/clan-search-stats-body/war-pie-chart/war-pie-chart.component';
 import {ClanMemberOverviewComponent} from './components/clan-search-stats/clan-search-stats-body/clan-member-overview/clan-member-overview.component';
 import {ChartsModule} from 'ng2-charts';
-import {UiSwitchModule} from 'angular2-ui-switch';
 import {CompleteClanMemberStatsService} from './services/complete-clan-member-stats/complete-clan-member-stats.service';
 import {TrophiesNightCellRendererComponent} from '../shared/components/ag-grid-cell-renderer/trophies-night-cell-renderer.component';
 import {TrophiesHomeCellRendererComponent} from '../shared/components/ag-grid-cell-renderer/trophies-home-cell-renderer.component';
@@ -20,6 +19,7 @@ import {LeagueBadgeCellRendererComponent} from '../shared/components/ag-grid-cel
 import {RoleCellRendererComponent} from '../shared/components/ag-grid-cell-renderer/role-cell-renderer-component';
 import {TownhallPictureCellRendererComponent} from '../shared/components/ag-grid-cell-renderer/townhall-picture-cell-renderer.component';
 import {WarStarsCellRendererComponent} from '../shared/components/ag-grid-cell-renderer/war-stars-cell-renderer.component';
+import {UiSwitchModule} from 'angular2-ui-switch';
 
 const appRoutes: Routes = [
   {path: 'clanSearch/:clanId', component: ClanSearchResultComponent, data: {depth: 3}}
